d472ee2b1013a543cb1a53dcd88627c3947e2006
Felipe Rodighero <feliperzarichta@gmail.com>
2024-01-31 14:46:08 -0300

    Templates create

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('HOME 1')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Felipe',
+    })
 
 
 def contato(request):
-    return HttpResponse('contato')
+    return render(request, 'recipes/contato.html')
 
 
 def sobre(request):
